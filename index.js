f71983b8d2f159fb22c55381b0e50a33a72098de
OgagaAkpowenre1 Daniel <ogagaogheneakpowenre@gmail.com>
2024-12-11 11:06:00 +0000

    Changes to be committed: 	modified:   index.js

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer')
 require('dotenv').config()
 
 var app = express();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads'); 
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname); 
+  }
+});
+
+const upload = multer({ storage: storage });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +23,18 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (req.file) {
+    const file = req.file;
+    res.json({
+      name: file.originalname, 
+      type: file.mimetype,     
+      size: file.size          
+    });
+  } else {
+    res.status(400).json({ error: 'No file uploaded' });
+  }
+})
 
 
 const port = process.env.PORT || 3000;
